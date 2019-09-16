c4ef9a7d64b5d4e10a9e95ba6b25bd5d71b74c43
Joey Sacino <46792454+RainyRam@users.noreply.github.com>
2019-09-15 23:03:33 -0600

    Add files via upload

---
+++ Assets/InputManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class InputManager : MonoBehaviour
+{
+
+    public Text outText;
+    public InputField textIn;
+    public Button enterButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        outText.text = "";
+    }
+
+    public void EnterPressed()
+    {
+        outText.text = Utilities.ProcessText(textIn.text);
+    }
+
+}
---
+++ Assets/Utilities.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Utilities
+{
+
+    public static string ProcessText(string input)
+    {
+
+        int words = 1;
+        int letters = 0;
+        double average;
+
+        foreach (char word in input)
+        {
+            if (char.IsSeparator(word))
+            {
+                words++;
+            }
+        }
+
+        foreach (char letter in input)
+        {
+            if (char.IsLetter(letter) && !char.IsSeparator(letter))
+            {
+                letters++;
+            }
+        }
+
+        average = letters / words;
+        return average.ToString();
+
+    }
+}
